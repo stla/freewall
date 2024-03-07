3df8a2b3b7c9189d1746231274d723152f77a2aa
stla <laurent_step@yahoo.fr>
2024-03-07 17:51:29 +0100

    first commit

---
+++ R/freewall.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+freewall <- function(message, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'freewall',
+    x,
+    width = width,
+    height = height,
+    package = 'freewall',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for freewall
+#'
+#' Output and render functions for using freewall within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a freewall
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name freewall-shiny
+#'
+#' @export
+freewallOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'freewall', width, height, package = 'freewall')
+}
+
+#' @rdname freewall-shiny
+#' @export
+renderFreewall <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, freewallOutput, env, quoted = TRUE)
+}
